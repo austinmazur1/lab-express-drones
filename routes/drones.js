9b1ace62c80facf5c2ca4a4a5daa07dee2e5be0a
Austin Mazur <mazuraustin1@gmail.com>
2023-04-10 10:23:55 +0200

    Finish up to iteration 4

--- routes/drones.js
+++ routes/drones.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require('../models/Drone.model')
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+router.get('/drones', async (req, res, next) => {
+  try {
+    const drones = await Drone.find()
+    console.log(drones)
+    res.render('drones/list', { drones })
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.get('/drones/create', async (req, res, next) => {
+  try {
+    const drones = await Drone.find()
+    res.render('drones/create-form', { drones })
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post('/drones/create', async (req, res, next) => {
+  const newDrone = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  };
+  await Drone.create(newDrone);
+  res.render('drones/list')
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,35 @@
 // Iteration #1
+const mongoose = require("mongoose");
+// const MONGO_URI = process.env.MONGO_DB_URI
+const Drone = require('../models/Drone.model')
+require('dotenv').config(); // Load environment variables from .env file
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+
+
+  mongoose
+  .connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(x => {
+    console.log(`Connected to Mongo database: "${x.connections[0].name}"`);
+
+    // Create new documents in the books collection
+    return Drone.create(drones);
+  })
+  .then(dronesFromDB => {
+    console.log(`Created ${dronesFromDB.length} drones`);
+
+    // Once the documents are created, close the DB connection
+    return mongoose.connection.close();
+  })
+  .then(() => {
+    // Once the DB connection is closed, print a message
+    console.log('DB connection closed!');
+  })
+  .catch(err => {
+    console.log(`An error occurred while creating movies from the DB: ${err}`);
+  });
